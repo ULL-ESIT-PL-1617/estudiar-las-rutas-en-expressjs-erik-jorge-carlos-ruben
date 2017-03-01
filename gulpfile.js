cecee617b7ee8fae41d65d9f8e1f688780ea8681
Rubén <alu0100309553@ull.edu.es>
2017-03-01 17:47:27 +0000

    Modificada estructura y añadir tareas a gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,15 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var ghPages = require('gulp-gh-pages');
+
+//gulp.task('default', ['github', 'build', 'deploy', 'iaas']);
+
+//gulp.task('github', shell.task(['git add .', 'git commit -m "Actualizando servidor"', 'git push origin master']));
+
+gulp.task('build', shell.task(['gitbook build ./docs ./public']));
+
+gulp.task('serve', shell.task(['gitbook serve ./docs']));
+
+//gulp.task('deploy', shell.task(['npm run deploy']));
+
+//gulp.task('iaas', shell.task(['npm run iaas']));
