9b0a4c69d68fa1fb21d74b2a5423c624ee314a2b
Rubén <alu0100309553@ull.edu.es>
2017-03-03 17:35:17 +0000

    subiendo a gh-pages

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require('express');
 var path = require('path');
 var port_iaas = 8080;
 var app = express();
-
+var basic = require('./src/basic');
+var routing = require('./src/routing');
 // HEROKU
 app.set('port', (process.env.PORT || 5000));
-
+// Servidor para libro
 app.get('/', function(pet, res){
-  res.sendfile(__dirname + 'public/_book/index.html');
+  res.sendfile(__dirname + '/public/index.html');
 });
 
-app.use(express.static(path.resolve('gh-pages')));
+app.use(express.static(path.resolve('public')));
 
+// Carga de los módulos de los códigos de ejemplo
+app.use('/basic', basic);
+app.use('/routing', routing);
+
+// Establecer puerto de escucha
 app.listen(app.get('port'));
 //app.listen(port_iaas);
